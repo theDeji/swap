c8ee4333998f93c285c2b2228746a56f91ed02e0
theDeji <emmi2110@gmail.com>
2020-07-15 00:15:02 -1200

    Updates

---
+++ precache-manifest.d9da97c082cec753cf6f98551f88c16d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9204843c19de446e75515e7ce7b0eb71",
+    "url": "/swap/index.html"
+  },
+  {
+    "revision": "9504e45beca2e578f9c1",
+    "url": "/swap/static/css/main.8425c022.chunk.css"
+  },
+  {
+    "revision": "242f19fdff09920b88a6",
+    "url": "/swap/static/js/2.04207f3b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/swap/static/js/2.04207f3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9504e45beca2e578f9c1",
+    "url": "/swap/static/js/main.5128b883.chunk.js"
+  },
+  {
+    "revision": "7bc1b5ec57225cf204a5",
+    "url": "/swap/static/js/runtime-main.12e83c91.js"
+  },
+  {
+    "revision": "a5ce4ce16cdba5141842cac165ca055e",
+    "url": "/swap/static/media/Apple.a5ce4ce1.svg"
+  },
+  {
+    "revision": "bda2c8e106d7b56d927dc2524373bb08",
+    "url": "/swap/static/media/Meet.bda2c8e1.png"
+  },
+  {
+    "revision": "b423d16073ae24ce411686ea4e2916a6",
+    "url": "/swap/static/media/Playstore.b423d160.svg"
+  }
+]);
